bd94a83bbb87ae91efe711df203bb608d3d714a1
lakshaydiwan <laxhay666@gmail.com>
2024-11-08 23:06:27 +0530

    fixed transition

--- app/components/CustomInput.tsx
+++ app/components/CustomInput.tsx
@@ -13,7 +13,7 @@ const CustomInput = ({type, placeholder, value, onChange}: InputProps) => {
     return (
         <div className="relative w-full">
             <input type={type} onChange={onChange} className="w-full pb-2 bg-inherit border-white focus:outline-none peer" />
-            <div className={`pointer-events-none ${value !== "" && "-translate-y-[18px] scale-90"} absolute bottom-3 left-0 peer-focus:scale-90 peer-focus:-translate-y-[18px] transition-all duration-300 ease-out`}>
+            <div className={`pointer-events-none ${value !== "" && "-translate-y-[18px] scale-90"} absolute bottom-3 left-0 peer-focus:scale-90 peer-focus:-translate-y-[18px] transition-all duration-500 ease-out`}>
                 {placeholder}
             </div>
             <div className='absolute bottom-0 left-0 w-full h-[1px] bg-white'>
